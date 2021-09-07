31c033e876ff85a067ff13bdb5044192d444c932
your_username <udaykirantutor@gmail.com>
2021-09-07 17:25:22 +0530

    my first commit

---
+++ src/Box.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Box() {
+    return (
+        <div>
+            Hello i am div 
+        </div>
+    )
+}
+
+export default Box
---
+++ src/Data.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { render } from "react-dom"
+
+function Data()
+{
+    return(
+        <div>
+            <h1>I am Data Componenet</h1>
+            <h2>Hello</h2>
+        </div>
+        
+    )
+}
+
+export default Data
+
+
+// parent element
+
